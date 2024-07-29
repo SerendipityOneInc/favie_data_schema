408ccdf6be8e1646a18f25ec2b6ca2ae3552584d
allenz-srp <allenz@srp.one>
2024-07-29 21:37:03 +0800

    rename package

--- convert.sh
+++ convert.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "合并avro代码..."
-python -m favie.tools.combine_schemas_with_unique_define --source-directory="avsc/interface/product/common" --output-file="avsc/interface/product/output/favie_product.avsc" --main-schema="favie.data.product.FavieProduct"
+python -m favie_data_schema.favie.tools.combine_schemas_with_unique_define --source-directory="avsc/interface/product/common" --output-file="avsc/interface/product/output/favie_product.avsc" --main-schema="favie.data.product.FavieProduct"
 echo "合并avro代码结束"
 
 echo "转化合并后的代码..."
-python -m favie.tools.avro_to_pydantic --avsc="avsc/interface/product/output/favie_product.avsc" --output-file="favie/data/interface/product/favie_product.py"
+python -m favie_data_schema.favie.tools.avro_to_pydantic --avsc="avsc/interface/product/output/favie_product.avsc" --output-file="favie_data_schema/favie/data/interface/product/favie_product.py"
 echo "转化合并后的代码结束"
