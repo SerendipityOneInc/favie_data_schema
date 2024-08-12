ada97650fefad810937fadff4a5d740a39199b8d
winston-srp <winston@srp.one>
2024-08-12 18:11:23 +0800

    5 update favie product schema (#21)
    * rename meta to metainfo
    
    * Auto-generate Pydantic models from AVSC
    
    * rename meta to metainfo
    
    * Auto-generate Pydantic models from AVSC
    
    * upadte avro_to_pydantic, change output List to Optional[List]
    
    * add deal List with Optional
    
    * update version
    
    * update version
    
    * rename simple product.item_id to sku_id
    
    * Auto-generate Pydantic models from AVSC
    
    * update simple product price type
    
    * Auto-generate Pydantic models from AVSC
    
    * update version
    
    * add best seller rank to product
    
    * delete seller_rank
    
    * update metainfo add source_type,parser_name,parser_time
    
    * update metainfo add source_type,parser_name,parser_time
    
    * Auto-generate Pydantic models from AVSC
    
    * add rpp to favie_product and modify meta_info times field
    
    * Auto-generate Pydantic models from AVSC
    
    * update verion to 0.0.9
    
    * rename favie_product field rpp to rrp
    
    * Auto-generate Pydantic models from AVSC
    
    * rename parsers_at to parses_at
    
    * Auto-generate Pydantic models from AVSC
    
    * add f_creates_at for favie_product
    
    * Auto-generate Pydantic models from AVSC
    
    * add f_creates_at for favie_product
    
    * add crawler adapter moudel
    
    ---------
    
    Co-authored-by: winston-srp <winston-srp@users.noreply.github.com>
    Co-authored-by: pangbaohui-dev <null>

---
+++ favie_data_schema/favie/adapter/common/common_utils.py
@@ -0,0 +1,29 @@
+import re
+
+class CommonUtils():
+    @staticmethod
+    def all_not_none(*args):
+        return all([arg is not None for arg in args])
+    
+    @staticmethod
+    def any_none(*args):
+        return any([arg is None for arg in args])
+    
+    @staticmethod
+    def all_none(*args):
+        return all([arg is None for arg in args])    
+    
+    @staticmethod
+    def host_trip_www(host:str):
+        return re.sub(r'^www\.', '', host) if host is not None else None
+
+
+if __name__ == "__main__":
+    print(CommonUtils.all_not_none(1, 2, 3))
+    print(CommonUtils.all_not_none(1, 2, None))
+    print(CommonUtils.any_none(1, 2, 3))
+    print(CommonUtils.any_none(1, 2, None))
+    print(CommonUtils.all_none(None, None, None))
+    print(CommonUtils.all_none(None, None, 1))
+
+
---
+++ favie_data_schema/favie/adapter/common/favie_product_status.py
@@ -0,0 +1,4 @@
+from enum import Enum,auto
+class FavieProductStatus(Enum):
+    SKU_NORMAL=auto()
+    SKU_NOT_FOUND=auto()
