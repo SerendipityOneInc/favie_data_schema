8fdb5acf7c774b1932a7eee8020661a63a7858f7
pangbaohui-dev <null>
2024-08-13 15:52:18 +0800

    add amazon review adapter

---
+++ favie_data_schema/favie/adapter/amazon/amazon_detail_convert.py
@@ -0,0 +1,241 @@
+from favie_data_schema.favie.adapter.common.favie_adapter import FavieProductAdapter
+from favie_data_schema.favie.data.interface.product.favie_product import *
+from favie_data_schema.favie.data.crawl_data.rainforest.rainforest_product_detail import RainforestProductDetail
+from favie_data_schema.favie.adapter.common.currency import CurrencyConverter
+from favie_data_schema.favie.adapter.common.favie_product_status import FavieProductStatus
+from favie_data_schema.favie.adapter.common.crawler_kakfa_message import CrawlerKafkaMessage
+from favie_data_schema.favie.adapter.common.common_utils import CommonUtils
+from favie_data_schema.favie.adapter.common.favie_product_utils import FavieProductUtils
+from favie_data_schema.favie.adapter.data_mock.amazon_message_read import read_amazon_message
+from datetime import datetime
+import logging
+
+from favie_data_schema.favie.data.interface.product.favie_review import FavieReview
+
+class AmazonDetailConvert():
+    @staticmethod
+    def convert_to_favie_product(amazon_message: CrawlerKafkaMessage) -> FavieProduct:
+        rainforest_product_detail = amazon_message.crawl_result
+        if not AmazonDetailConvert.rainforest_product_detail_check(rainforest_product_detail):
+            logging.error("rainforest_product_detail is invalid: %s", rainforest_product_detail)
+            return None
+        favie_product = FavieProduct()
+        favie_product.sku_id = rainforest_product_detail.product.asin
+        favie_product.spu_id = rainforest_product_detail.product.parent_asin
+        favie_product.site = CommonUtils.host_trip_www(amazon_message.host)
+        favie_product.title = rainforest_product_detail.product.title
+        favie_product.link = rainforest_product_detail.product.link
+        favie_product.sub_title = rainforest_product_detail.product.sub_title.text if rainforest_product_detail.product.sub_title is not None else None
+        favie_product.sub_title_link = rainforest_product_detail.product.sub_title.link if rainforest_product_detail.product.sub_title is not None else None
+        favie_product.description = rainforest_product_detail.product.description
+        favie_product.description_external_link = None
+        favie_product.rich_product_description = None
+        favie_product.price = AmazonDetailConvert.get_price(rainforest_product_detail)
+        favie_product.rrp = AmazonDetailConvert.get_rrp(rainforest_product_detail)
+        favie_product.images = AmazonDetailConvert.get_images(rainforest_product_detail)
+        favie_product.f_images = None
+        favie_product.videos = AmazonDetailConvert.get_videos(rainforest_product_detail)
+        favie_product.categories = AmazonDetailConvert.get_categories(rainforest_product_detail)
+        favie_product.f_categories = None
+        favie_product.brand = AmazonDetailConvert.get_brand(rainforest_product_detail)
+        favie_product.f_brand = None
+        favie_product.feature_bullets = rainforest_product_detail.product.feature_bullets        
+        favie_product.attributes = AmazonDetailConvert.get_attributes(rainforest_product_detail)
+        favie_product.specifications = AmazonDetailConvert.get_specifications(rainforest_product_detail)
+        favie_product.standard_attributes = AmazonDetailConvert.get_standard_attributes(rainforest_product_detail)
+        favie_product.offers = None
+        favie_product.seller = AmazonDetailConvert.get_seller(rainforest_product_detail)
+        favie_product.inventory = None
+        favie_product.keywords = rainforest_product_detail.product.keywords
+        favie_product.search_alias = None
+        favie_product.deal = AmazonDetailConvert.get_deal(rainforest_product_detail)
+        favie_product.shipping = None
+        favie_product.fulfillment = None
+        favie_product.returns_policy = None
+        favie_product.variants = None
+        favie_product.f_sku_id = FavieProductUtils.gen_f_sku_id(favie_product)
+        favie_product.f_spu_id = FavieProductUtils.gen_f_spu_id(favie_product)
+        favie_product.promotion = AmazonDetailConvert.get_promotion(rainforest_product_detail)
+        favie_product.f_meta = MetaInfo(
+            source_type = str(amazon_message.source),
+            parser_name = f"{amazon_message.parser_name}-adapter",
+            parses_at = str(int(datetime.now().timestamp()))
+        )
+        favie_product.f_status = FavieProductStatus.SKU_NORMAL.name
+        return favie_product
+    
+        
+
+    @staticmethod
+    def get_best_seller_rank(rainforest_product_detail: RainforestProductDetail):
+        if rainforest_product_detail.product.bestsellers_rank is not None and len(rainforest_product_detail.product.bestsellers_rank) > 0:
+            ranks = [SellerRank(
+                category = x.category,
+                rank = x.rank,
+                link = x.link
+                ) for x in rainforest_product_detail.product.bestsellers_rank if CommonUtils.all_not_none(x.category,x.rank)]
+            return ranks if len(ranks) > 0 else None
+        return None
+
+    @staticmethod
+    def get_promotion(rainforest_product_detail: RainforestProductDetail):
+        if rainforest_product_detail.product.promotions is not None and len(rainforest_product_detail.product.promotions) > 0:
+            promotion = Promotion(why_buy=[x.name for x in rainforest_product_detail.product.promotions if x.name is not None])
+            return promotion if len(promotion.why_buy) > 0 else None
+        return None
+
+    @staticmethod
+    def get_variants(rainforest_product_detail: RainforestProductDetail):
+        if(rainforest_product_detail.product.variants is not None and len(rainforest_product_detail.product.variants) > 0): 
+            variants = [SimpleProduct(
+                    sku_id=x.asin,
+                    title=None, #variants 的schema定义有误，缺少title字段，多了一个text字段
+                    link=x.link,
+                    price=AmazonDetailConvert.convert_price(x.price)
+                ) for x in rainforest_product_detail.product.variants if x.asin is not None]
+            return variants if len(variants) > 0 else None
+        return None
+    
+    @staticmethod
+    def convert_price(amazon_price)->Price:
+        if amazon_price is None:
+            return None
+        currency_converter = CurrencyConverter(amazon_price.currency,amazon_price.value)
+        price = Price(
+                currency=currency_converter.get_currency_code(),
+                value = currency_converter.get_subunit_value(),
+                updates_at=str(int(datetime.now().timestamp()))
+            )
+        return price if CommonUtils.all_not_none(price.currency,price.value) else None
+
+    @staticmethod
+    def get_deal(rainforest_product_detail: RainforestProductDetail):
+        if(rainforest_product_detail.product.buybox_winner is not None and rainforest_product_detail.product.buybox_winner.deal is not None):
+            was_price = AmazonDetailConvert.convert_price(rainforest_product_detail.product.buybox_winner.price)
+            if(was_price is None):
+                return None
+            
+            claimed = rainforest_product_detail.product.buybox_winner.deal.claimed
+            percentage = claimed.percentage if claimed is not None else None
+            if(percentage is None):
+                return None
+            
+            deal_price = Price(
+                    currency=was_price.currency,
+                    value = int(was_price.value * percentage / 100),
+                    updates_at=str(int(datetime.now().timestamp()))
+            )
+            return Deal(
+                was_price = was_price,
+                deal_price = deal_price ,
+                description = rainforest_product_detail.product.buybox_winner.deal.claimed.raw,
+                ends_at= rainforest_product_detail.product.buybox_winner.deal.timing.ends_at
+            )
+        return None
+    
+    @staticmethod
+    def get_seller(rainforest_product_detail: RainforestProductDetail):
+        if(rainforest_product_detail.product.buybox_winner is not None):
+            return Seller(
+                id=rainforest_product_detail.product.buybox_winner.offer_id
+            )
+        return None
+    
+    @staticmethod
+    def get_standard_attributes(rainforest_product_detail: RainforestProductDetail):
+        return None
+    
+    @staticmethod
+    def get_specifications(rainforest_product_detail: RainforestProductDetail):
+        if rainforest_product_detail.product.specifications is not None and len(rainforest_product_detail.product.specifications) > 0:
+            specifications = [AttributeItem(
+                name = x.name,
+                value = x.value
+            ) for x in rainforest_product_detail.product.specifications if CommonUtils.all_not_none(x.name,x.value)]
+            return specifications if len(specifications) > 0 else None
+        return None
+    
+    @staticmethod
+    def get_attributes(rainforest_product_detail: RainforestProductDetail):
+        if rainforest_product_detail.product.attributes is not None and len(rainforest_product_detail.product.attributes) > 0:
+            attributes = [AttributeItem(
+                name = x.name,
+                value = x.value
+            ) for x in rainforest_product_detail.product.attributes if CommonUtils.all_not_none(x.name,x.value)] 
+            return attributes if len(attributes) > 0 else None
+        return None
+    
+    @staticmethod
+    def get_brand(rainforest_product_detail: RainforestProductDetail):
+        if rainforest_product_detail.product.brand is not None:
+            return Brand(
+                name = rainforest_product_detail.product.brand
+            )
+        return None
+    
+    @staticmethod
+    def get_categories(rainforest_product_detail: RainforestProductDetail):
+        if rainforest_product_detail.product.categories is not None and len(rainforest_product_detail.product.categories) > 0:
+            categories = [ CategoryItem(
+                id = x.category_id,
+                name = x.name,
+                ) for x in rainforest_product_detail.product.categories if x.category_id is not None]
+            return categories if len(categories) > 0 else None
+        return None
+
+    @staticmethod
+    def get_price(rainforest_product_detail: RainforestProductDetail):
+        if rainforest_product_detail.product.buybox_winner is not None:
+            return AmazonDetailConvert.convert_price(rainforest_product_detail.product.buybox_winner.price)
+        return None
+    
+    @staticmethod
+    def get_rrp(rainforest_product_detail: RainforestProductDetail):
+        if rainforest_product_detail.product.buybox_winner is not None:
+            return AmazonDetailConvert.convert_price(rainforest_product_detail.product.buybox_winner.rrp)
+        return None
+    
+    @staticmethod
+    def get_images(rainforest_product_detail: RainforestProductDetail):
+        if rainforest_product_detail.product.images is not None or rainforest_product_detail.product.main_image is not None:
+            images = Images(
+                main_image = rainforest_product_detail.product.main_image.link if rainforest_product_detail.product.main_image is not None else None,
+                images = [x.link for x in rainforest_product_detail.product.images if x.link is not None]
+            )
+            return images if images.main_image is not None or len(images.image_list) > 0 else None
+        return None
+    
+    @staticmethod
+    def get_videos(rainforest_product_detail: RainforestProductDetail):
+        if rainforest_product_detail.product.videos is not None and len(rainforest_product_detail.product.videos) > 0:
+            videos = [
+                Video(
+                    link = x.url,
+                    title = x.title,
+                    duration_seconds= x.duration_seconds,
+                    thumbnail = x.thumbnail,
+                    height= x.height,
+                    width= x.width,
+                    is_hero_video= x.is_hero_video
+                ) 
+                for x in rainforest_product_detail.product.videos
+                if x.url is not None    
+            ]
+            return videos if len(videos) > 0 else None
+        return None
+    
+    @staticmethod
+    def rainforest_product_detail_check(rainforest_product_detail: RainforestProductDetail) -> bool:
+        if rainforest_product_detail is None:
+            return False
+        if rainforest_product_detail.product is None:
+            return False
+        return True
+        
+def main():
+    amazon_message = read_amazon_message("/Users/pangbaohui/workspace-srp/favie_data_schema/favie_data_schema/favie/resources/amazon_message.json")
+    favie_product = AmazonDetailConvert.convert_to_favie_product(amazon_message)
+    print(favie_product.model_dump_json(exclude_none = True) if favie_product else None)
+
+if __name__ == "__main__":
+    main()
---
+++ favie_data_schema/favie/adapter/amazon/amazon_review_convert.py
@@ -0,0 +1,58 @@
+from favie_data_schema.favie.data.interface.product.favie_product import *
+from favie_data_schema.favie.data.crawl_data.rainforest.rainforest_product_detail import RainforestProductDetail, TopReviews
+from favie_data_schema.favie.adapter.common.crawler_kakfa_message import CrawlerKafkaMessage
+from favie_data_schema.favie.adapter.data_mock.amazon_message_read import read_amazon_message
+import logging
+
+from favie_data_schema.favie.data.interface.product.favie_review import FavieReview
+
+class AmazoneReviewConvert():
+    @staticmethod
+    def convert_to_favie_review(crawler_kafka_message: CrawlerKafkaMessage) -> list[FavieReview]:
+        if not AmazoneReviewConvert.__check(crawler_kafka_message):
+            logging.error(f"Invalid crawler_kafka_message : {crawler_kafka_message}")
+            return None
+        reviews = crawler_kafka_message.crawl_result.product.top_reviews
+        favie_reviews:List[FavieReview] = [AmazoneReviewConvert.__convert_to_favie_review(x) for x in reviews if x is not None] if reviews is not None else None
+        return favie_reviews if len(favie_reviews) > 0 else None
+    
+    def __convert_to_favie_review(review: TopReviews)->FavieReview:
+        if(review is None): 
+            return None
+        favie_review = FavieReview()
+        favie_review.author_id = review.profile.id
+        favie_review.author_name = review.profile.name
+        favie_review.author_url = review.profile.link
+        favie_review.review_id = review.id
+        favie_review.link = review.link
+        favie_review.body = review.body
+        favie_review.title = review.title
+        favie_review.helpful_votes = review.helpful_votes
+        favie_review.vine_program = review.vine_program
+        favie_review.verified_purchase = review.verified_purchase
+        favie_review.is_global_review = review.is_global_review
+        favie_review.review_country = review.review_country
+        return favie_review
+    
+    @staticmethod
+    def __check(crawler_kafka_message: RainforestProductDetail):
+        if(crawler_kafka_message is None):
+            return False
+        if(crawler_kafka_message.crawl_result is None):
+            return False
+        if(crawler_kafka_message.crawl_result.product is None):
+            return False
+        if(crawler_kafka_message.crawl_result.product.top_reviews is None or len(crawler_kafka_message.crawl_result.product.top_reviews) == 0):
+            return False
+        return True
+    
+def main():
+    amazon_message = read_amazon_message("/Users/pangbaohui/workspace-srp/favie_data_schema/favie_data_schema/favie/resources/amazon_message.json")
+    favie_reviews: list[FavieReview] = AmazoneReviewConvert.convert_to_favie_review(amazon_message)
+    if favie_reviews is not None:
+        for favie_review in favie_reviews:
+            if(favie_review is not None):
+                print(favie_review.model_dump_json())
+
+if __name__ == "__main__":
+    main()
