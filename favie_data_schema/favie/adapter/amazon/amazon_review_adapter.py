2c52b18d44ab5972687b57c03af44b69c90f45fc
pangbaohui-dev <null>
2024-08-15 15:58:25 +0800

    rename amazone to amazon

--- favie_data_schema/favie/adapter/amazon/amazon_review_adapter.py
+++ favie_data_schema/favie/adapter/amazon/amazon_review_adapter.py
@@ -5,16 +5,16 @@ from favie_data_schema.favie.data.crawl_data.rainforest.rainforest_product_detai
 from favie_data_schema.favie.adapter.common.crawler_kakfa_message import CrawlerKafkaMessage
 from favie_data_schema.favie.adapter.common.favie_product_utils import FavieProductUtils
 from favie_data_schema.favie.adapter.amazon.amazon_detail_convert import AmazonDetailConvert
-from favie_data_schema.favie.adapter.amazon.amazon_review_convert import AmazoneReviewConvert
+from favie_data_schema.favie.adapter.amazon.amazon_review_convert import AmazonReviewConvert
 from favie_data_schema.favie.adapter.data_mock.amazon_message_read import read_amazon_message
 import logging
 
 from favie_data_schema.favie.data.interface.product.favie_review import FavieReview
 
-class AmazoneReviewAdapter(FavieReviewAdapter):
+class AmazonReviewAdapter(FavieReviewAdapter):
     @staticmethod
     def convert_to_favie_review(crawler_kafka_message: CrawlerKafkaMessage) -> list[FavieReview]:
-        favie_reviews = AmazoneReviewConvert.convert_to_favie_review(crawler_kafka_message)
+        favie_reviews = AmazonReviewConvert.convert_to_favie_review(crawler_kafka_message)
         if favie_reviews is None:
             return None
         
@@ -34,7 +34,9 @@ class AmazoneReviewAdapter(FavieReviewAdapter):
     
 def main():
     amazon_message = read_amazon_message("/Users/pangbaohui/workspace-srp/favie_data_schema/favie_data_schema/favie/resources/bug.json")
-    favie_reviews = AmazoneReviewAdapter.convert_to_favie_review(amazon_message)
+    favie_reviews = AmazonReviewAdapter.convert_to_favie_review(amazon_message)
+    if favie_reviews is None:
+        return None
     for favie_review in favie_reviews:
         print(favie_review.model_dump_json())
 
