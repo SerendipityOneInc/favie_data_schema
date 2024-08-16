fdda0249dff9a5829c047a36b38f048f77b93bb8
winston-srp <winston@srp.one>
2024-08-16 10:02:28 +0800

    5 update favie product schema (#31)
    * rename meta to metainfo
    
    * Auto-generate Pydantic models from AVSC
    
    * rename meta to metainfo
    
    * Auto-generate Pydantic models from AVSC
    
    * upadte avro_to_pydantic, change output List to Optional[List]
    
    * add deal List with Optional
    
    * update version
    
    * update version
    
    * rename simple product.item_id to sku_id
    
    * Auto-generate Pydantic models from AVSC
    
    * update simple product price type
    
    * Auto-generate Pydantic models from AVSC
    
    * update version
    
    * add best seller rank to product
    
    * delete seller_rank
    
    * update metainfo add source_type,parser_name,parser_time
    
    * update metainfo add source_type,parser_name,parser_time
    
    * Auto-generate Pydantic models from AVSC
    
    * add rpp to favie_product and modify meta_info times field
    
    * Auto-generate Pydantic models from AVSC
    
    * update verion to 0.0.9
    
    * rename favie_product field rpp to rrp
    
    * Auto-generate Pydantic models from AVSC
    
    * rename parsers_at to parses_at
    
    * Auto-generate Pydantic models from AVSC
    
    * add f_creates_at for favie_product
    
    * Auto-generate Pydantic models from AVSC
    
    * add f_creates_at for favie_product
    
    * add crawler adapter moudel
    
    * define FavieAdapter interface
    
    * add review_summary to favie_product
    
    * add review_summary to favie_product
    
    * add amazon review adapter
    
    * bugfix amazon_detail_convert none exception
    
    * bugfix amazon_detail_convert none exception
    
    * update version to 0.0.16
    
    * bugfix : filter review is none
    
    * rename amazone to amazon
    
    * rename amazone to amazon
    
    * fixbug : len none fail,add list_len to common_utils
    
    * add media interface for image
    
    * add media interface for image
    
    ---------
    
    Co-authored-by: winston-srp <winston-srp@users.noreply.github.com>
    Co-authored-by: pangbaohui-dev <null>

---
+++ favie_data_schema/favie/data/interface/common/favie_enum.py
@@ -0,0 +1,6 @@
+from enum import IntEnum
+
+class FavieSourceType(IntEnum):
+    PRODUCT_DETAIL:1
+    PRODUCT_REVIEW:2
+    WEB_PAGE:3
---
+++ favie_data_schema/favie/data/interface/media/favie_image_crawl_request.py
@@ -0,0 +1,13 @@
+from typing import List, Optional
+from pydantic import BaseModel, Field
+
+class ImageItem(BaseModel):
+    outer_key: Optional[str] = None
+    url: Optional[str] = None
+    text: Optional[str] = None
+
+class FavieImageCrawlRequest(BaseModel):
+    source_type: Optional[int] = None
+    source_id: Optional[str] = None
+    images: Optional[List[ImageItem]] = None
+
