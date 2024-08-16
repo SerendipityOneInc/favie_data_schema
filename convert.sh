041941473700da332102b9c01d1e005335c6907a
pangbaohui-dev <null>
2024-08-16 10:16:50 +0800

    add image crawl response

--- convert.sh
+++ convert.sh
@@ -6,6 +6,7 @@ python -m favie_data_schema.favie.tools.combine_schemas_with_unique_define --sou
 python -m favie_data_schema.favie.tools.combine_schemas_with_unique_define --source-directory="avsc/interface/product/source_code" --output-file="avsc/interface/product/output_code/favie_review.avsc" --main-schema="favie.data.interface.product.FavieReview"
 python -m favie_data_schema.favie.tools.combine_schemas_with_unique_define --source-directory="avsc/interface/media/source_code" --output-file="avsc/interface/media/output_code/favie_image.avsc" --main-schema="favie.data.interface.media.FavieImage"
 python -m favie_data_schema.favie.tools.combine_schemas_with_unique_define --source-directory="avsc/interface/media/source_code" --output-file="avsc/interface/media/output_code/favie_image_crawl_request.avsc" --main-schema="favie.data.interface.media.FavieImageCrawlRequest"
+python -m favie_data_schema.favie.tools.combine_schemas_with_unique_define --source-directory="avsc/interface/media/source_code" --output-file="avsc/interface/media/output_code/favie_image_crawl_response.avsc" --main-schema="favie.data.interface.media.FavieImageCrawlResponse"
 echo "合并avro代码结束"
 
 echo "转化合并后的代码..."
@@ -13,4 +14,5 @@ python -m favie_data_schema.favie.tools.avro_to_pydantic --avsc="avsc/interface/
 python -m favie_data_schema.favie.tools.avro_to_pydantic --avsc="avsc/interface/product/output_code/favie_review.avsc" --output-file="favie_data_schema/favie/data/interface/product/favie_review.py"
 python -m favie_data_schema.favie.tools.avro_to_pydantic --avsc="avsc/interface/media/output_code/favie_image.avsc" --output-file="favie_data_schema/favie/data/interface/media/favie_image.py"
 python -m favie_data_schema.favie.tools.avro_to_pydantic --avsc="avsc/interface/media/output_code/favie_image_crawl_request.avsc" --output-file="favie_data_schema/favie/data/interface/media/favie_image_crawl_request.py"
+python -m favie_data_schema.favie.tools.avro_to_pydantic --avsc="avsc/interface/media/output_code/favie_image_crawl_response.avsc" --output-file="favie_data_schema/favie/data/interface/media/favie_image_crawl_response.py"
 echo "转化合并后的代码结束"
