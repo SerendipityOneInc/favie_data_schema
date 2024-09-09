ea820fbb0197b3870c8d077b2dd1bb6c397e6a56
pangbaohui-dev <null>
2024-09-09 12:02:52 +0800

    0.0.50.alpha.8

---
+++ favie_data_schema/__init__.py
@@ -0,0 +1,5 @@
+from .favie.data import crawl_data
+from .favie.data import interface
+from .favie import adapter
+
+__all__ = ["interface", "adapter","crawl_data"]
--- favie_data_schema/favie/__init__.py
+++
@@ -1,5 +0,0 @@
-from .data import crawl_data
-from .data import interface
-from . import adapter
-
-__all__ = ["interface", "adapter","crawl_data"]
