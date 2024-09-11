c0a43cf97be8f795b378ff1c7d5e688d305bec91
pangbaohui-dev <null>
2024-09-11 10:57:20 +0800

    add f_meta to product review

--- favie_data_schema/favie/data/interface/product/favie_product_review.py
+++ favie_data_schema/favie/data/interface/product/favie_product_review.py
@@ -1,6 +1,19 @@
 from typing import List, Optional
 from pydantic import BaseModel, Field
 
+class MetaInfo(BaseModel):
+    source_type: Optional[str] = None
+    parser_name: Optional[str] = None
+    parses_at: Optional[str] = None
+    f_categories_update_at: Optional[str] = None
+    f_images_crawl_send_at: Optional[str] = None
+    f_categories_map_success: Optional[bool] = None
+    source_1_updates_at: Optional[str] = None
+    source_2_updates_at: Optional[str] = None
+    source_3_updates_at: Optional[str] = None
+    source_4_updates_at: Optional[str] = None
+    source_5_updates_at: Optional[str] = None
+
 class FavieProductReview(BaseModel):
     f_review_id: Optional[str] = None
     f_spu_id: Optional[str] = None
@@ -23,6 +36,7 @@ class FavieProductReview(BaseModel):
     position: Optional[int] = None
     helpful_votes: Optional[int] = None
     unhelpful_votes: Optional[int] = None
+    f_meta: Optional[MetaInfo] = None
     f_updates_at: Optional[str] = None
     f_creates_at: Optional[str] = None
 
