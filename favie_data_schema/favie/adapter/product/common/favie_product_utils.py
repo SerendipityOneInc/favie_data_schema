de761198cd838810ed4eb0a644c4a6cb01d9f971
pangbaohui-dev <null>
2024-09-13 10:15:06 +0800

    add gen_f_id method to favie_product_utils

--- favie_data_schema/favie/adapter/product/common/favie_product_utils.py
+++ favie_data_schema/favie/adapter/product/common/favie_product_utils.py
@@ -6,24 +6,27 @@ class FavieProductUtils():
     def gen_f_sku_id(product:FavieProductDetail):
         if(product is None):
             return None
-        if(CommonUtils.all_not_none(product.sku_id,product.site)):
-            return f"{product.sku_id}-{product.site}"
-        return None
+        return FavieProductUtils.gen_f_id(id=product.sku_id,site=product.site)
 
     @staticmethod
     def gen_f_spu_id(product: FavieProductDetail):
         if(product is None):
             return None
         spu_id = product.spu_id if product.spu_id is not None else product.sku_id
-        if(CommonUtils.all_not_none(spu_id,product.site)):
-            return f"{spu_id}-{product.site}"
+        return FavieProductUtils.gen_f_id(id=spu_id,site=product.site)
         
     @staticmethod
-    def gen_review_id(review: FavieProductReview):
+    def gen_f_review_id(review: FavieProductReview):
         if(review.site is None):
             return None
         review_id = review.review_id if review.review_id else CommonUtils.md5_hash(f'{review.author_id}-{review.author_name}-{review.link}-{review.position}')
-        return f"{review_id}-{review.site}"
+        return FavieProductUtils.gen_f_id(id=review_id,site=review.site)
+    
+    @staticmethod
+    def gen_f_id(*,id:str,site:str):
+        if CommonUtils.any_none(id,site):
+            return None
+        return f"{id}-{site}"
     
     @staticmethod
     def get_product_price(product):
