3e034c6c8032476c55cdf63e8daf20a0295551ee
kyle-srp <kyle@srp.one>
2024-09-25 11:42:08 +0800

    feat: support product list data and fix timezone problem (#55)

--- favie_data_schema/favie/data/crawl_data/crawler/favie_spider_product_list_data.py
+++ favie_data_schema/favie/data/crawl_data/crawler/favie_spider_product_list_data.py
@@ -5,26 +5,24 @@ from typing import Optional
 from pydantic import BaseModel, Field
 
 from favie_data_schema.favie.data.crawl_data.crawler.common import Source
-from favie_data_schema.favie.data.crawl_data.rainforest.rainforest_product_detail import (
-    RainforestProductDetail,
+from favie_data_schema.favie.data.crawl_data.crawler.stark_product_list import (
+    StarkProductList,
 )
 
 
-class FavieSpiderProductData(BaseModel):
+class FavieSpiderProductListData(BaseModel):
     id: str = uuid.uuid1().hex  # UUID1
-    url: str  # URL
-    host: str  # 商品所在网站的域名，比如：www.amazon.com
-    product_id: Optional[str] = None  # 商品ID，比如：Amazon为ASIN: B0CBLKS51N
-    product_title: Optional[str] = None  # 商品Title
     parser_name: Optional[str] = None  # used for spider
+    category_id: Optional[str] = None  # 类目id
+    page: Optional[int] = None  # 页码
     source: Optional[Source] = 0  # 数据来源
+    mission: Optional[str] = None  # 业务上的任务概念
+    task_id: Optional[int] = None  # 爬虫任务ID
     raw_result: Optional[
         str
     ] = None  # 原始抓取结果，如果为Rainforest则为API则为Response的json结果，如果为自研爬虫则为原始html
     crawl_result: Optional[
-        RainforestProductDetail
-    ] = None  # 商品解析结果，存储为JSON格式，遵循RainForest的数据模型
-    mission: Optional[str] = None  # 业务上的任务概念
-    task_id: Optional[int] = None  # 爬虫任务ID
+        StarkProductList
+    ] = None  # 类目解析结果，存储为JSON格式，遵循RainForest的数据模型
     create_time: Optional[datetime] = Field(default_factory=lambda: datetime.now(datetime.timezone.utc))  # 爬虫写入时间，使用UTC
     update_time: Optional[datetime] = Field(default_factory=lambda: datetime.now(datetime.timezone.utc))  # 数据更新时间，使用UTC
