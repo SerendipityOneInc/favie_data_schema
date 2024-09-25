37d8cc0fb3ce6623c9b78ac2b83167452c158497
winston-srp <winston@srp.one>
2024-09-25 20:57:29 +0800

    rename variarble

--- favie_data_schema/favie/adapter/product/product_list_adapter/stark_product_list_adapter.py
+++ favie_data_schema/favie/adapter/product/product_list_adapter/stark_product_list_adapter.py
@@ -7,8 +7,8 @@ from favie_data_schema.favie.adapter.product.common.currency import CurrencyConv
 from favie_data_schema.favie.adapter.product.common.favie_product_adapter import FavieProductDetailAdapter
 from favie_data_schema.favie.adapter.tools.data_mock_read import read_object
 from favie_data_schema.favie.data.crawl_data.crawler.common import Source
-from favie_data_schema.favie.data.crawl_data.crawler.stark_product_list import Price as AmazonPrice
-from favie_data_schema.favie.data.crawl_data.crawler.stark_product_list import StarkProductList
+from favie_data_schema.favie.data.crawl_data.crawler.stark_product_list import Price as StarkPrice
+from favie_data_schema.favie.data.crawl_data.crawler.stark_product_list import ProductListItem
 from favie_data_schema.favie.data.interface.product.favie_product import *
 
 
@@ -22,18 +22,18 @@ class StarkProductListAdapter(FavieProductDetailAdapter):
         if CommonUtils.list_len(stark_product_list) == 0:
             return None
 
-        favie_product_list = []
-        for stark_product in stark_product_list:
+        favie_product_list: List[ProductListItem] = []
+        for stark_product_item in stark_product_list:
             favie_product = FavieProductDetail()
             parse_time = StarkMessageUtils.get_parse_time(message)
-            favie_product.sku_id = stark_product.asin
+            favie_product.sku_id = stark_product_item.asin
             favie_product.site = StarkMessageUtils.get_domain(message)
-            favie_product.title = stark_product.title
-            favie_product.link = stark_product.link
-            favie_product.price = StarkProductListAdapter.get_price(stark_product, parse_time)
-            favie_product.rrp = StarkProductListAdapter.get_rrp(stark_product, parse_time)
-            favie_product.images = StarkProductListAdapter.get_images(stark_product)
-            favie_product.review_summary = StarkProductListAdapter.get_review_summary(stark_product)
+            favie_product.title = stark_product_item.title
+            favie_product.link = stark_product_item.link
+            favie_product.price = StarkProductListAdapter.get_price(stark_product_item, parse_time)
+            favie_product.rrp = StarkProductListAdapter.get_rrp(stark_product_item, parse_time)
+            favie_product.images = StarkProductListAdapter.get_images(stark_product_item)
+            favie_product.review_summary = StarkProductListAdapter.get_review_summary(stark_product_item)
             favie_product.f_meta = MetaInfo(
                 source_type=Source.SPIDER,
                 parser_name=message.parser_name,
@@ -44,31 +44,31 @@ class StarkProductListAdapter(FavieProductDetailAdapter):
         return favie_product_list if CommonUtils.list_len(favie_product_list) > 0 else None
 
     @staticmethod
-    def get_review_summary(crawl_result: StarkProductList) -> ReviewSummary:
-        return ReviewSummary(rating=crawl_result.rating, ratings_total=crawl_result.ratings_total)
+    def get_review_summary(stark_product_item: ProductListItem) -> ReviewSummary:
+        return ReviewSummary(rating=stark_product_item.rating, ratings_total=stark_product_item.ratings_total)
 
     @staticmethod
-    def get_images(crawl_result: StarkProductList) -> Images:
-        return Images(main_image=crawl_result.image) if crawl_result.image is not None else None
+    def get_images(stark_product_item: ProductListItem) -> Images:
+        return Images(main_image=stark_product_item.image) if stark_product_item.image is not None else None
 
     @staticmethod
-    def get_price(crawl_result: StarkProductList, parse_time: str) -> Price:
-        amazon_price = crawl_result.price if crawl_result.price is not None else None
-        if amazon_price is None and CommonUtils.list_len(crawl_result.prices) > 0:
-            amazon_price = next(filter(lambda price: price.is_primary, crawl_result.prices))
-        return StarkProductListAdapter.convert_price(amazon_price, parse_time)
+    def get_price(stark_product_item: ProductListItem, parse_time: str) -> Price:
+        stark_price = stark_product_item.price if stark_product_item.price is not None else None
+        if stark_price is None and CommonUtils.list_len(stark_product_item.prices) > 0:
+            stark_price = next(filter(lambda price: price.is_primary, stark_product_item.prices))
+        return StarkProductListAdapter.convert_price(stark_price, parse_time)
 
     @staticmethod
-    def get_rrp(crawl_result: StarkProductList, parse_time: str) -> Price:
-        if CommonUtils.list_len(crawl_result.prices) > 0:
-            amazon_price = next(filter(lambda price: price.is_rrp, crawl_result.prices))
-            return StarkProductListAdapter.convert_price(amazon_price, parse_time)
+    def get_rrp(stark_product_item: ProductListItem, parse_time: str) -> Price:
+        if CommonUtils.list_len(stark_product_item.prices) > 0:
+            stark_price = next(filter(lambda price: price.is_rrp, stark_product_item.prices))
+            return StarkProductListAdapter.convert_price(stark_price, parse_time)
 
     @staticmethod
-    def convert_price(amazon_price: AmazonPrice, parse_time: str) -> Price:
-        if amazon_price is None:
+    def convert_price(stark_price: StarkPrice, parse_time: str) -> Price:
+        if stark_price is None:
             return None
-        currency_converter = CurrencyConverter(amazon_price.currency, amazon_price.value)
+        currency_converter = CurrencyConverter(stark_price.currency, stark_price.value)
         price = Price(
             currency=currency_converter.get_currency_code(),
             value=currency_converter.get_subunit_value(),
