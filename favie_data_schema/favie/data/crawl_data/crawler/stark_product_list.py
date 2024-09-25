2feb3f74529a130fa9401796d0ac3ceae8042ff2
winston-srp <winston@srp.one>
2024-09-25 21:38:53 +0800

    add asin to Price

--- favie_data_schema/favie/data/crawl_data/crawler/stark_product_list.py
+++ favie_data_schema/favie/data/crawl_data/crawler/stark_product_list.py
@@ -1,13 +1,17 @@
 from typing import List, Optional
-from pydantic import BaseModel, Field
+
+from pydantic import BaseModel
+
 
 class Category(BaseModel):
     name: Optional[str] = None
     id: Optional[str] = None
 
+
 class Bestseller(BaseModel):
     link: Optional[str] = None
     category: Optional[str] = None
+
 
 class Price(BaseModel):
     symbol: Optional[str] = None
@@ -15,8 +19,10 @@ class Price(BaseModel):
     currency: Optional[str] = None
     raw: Optional[str] = None
     name: Optional[str] = None
+    asin: Optional[str] = None
     is_primary: Optional[bool] = None
     is_rrp: Optional[bool] = None
+
 
 class DealListItem(BaseModel):
     position: Optional[int] = None
@@ -34,7 +40,8 @@ class DealListItem(BaseModel):
     percent_off: Optional[int] = None
     deal_type: Optional[str] = None
     is_lightning_deal: Optional[bool] = None
-        
+
+
 class ProductListItem(BaseModel):
     position: Optional[int] = None
     title: Optional[str] = None
@@ -44,11 +51,11 @@ class ProductListItem(BaseModel):
     image: Optional[str] = None
     rating: Optional[float] = None
     ratings_total: Optional[int] = None
-    prices: Optional[List[Price]] = None  #only support rf api
+    prices: Optional[List[Price]] = None  # only support rf api
     price: Optional[Price] = None
-    rrp:Optional[Price] = None
-    
+    rrp: Optional[Price] = None
+
+
 class StarkProductList(BaseModel):
     product_list: Optional[List[ProductListItem]] = None
     deal_list: Optional[List[DealListItem]] = None
-
