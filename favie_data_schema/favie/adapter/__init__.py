ad81b97cd41fa45cc7831637457c0fa5e5ead666
winston-srp <winston@srp.one>
2024-09-30 11:20:15 +0800

    update data-common version

--- favie_data_schema/favie/adapter/__init__.py
+++ favie_data_schema/favie/adapter/__init__.py
@@ -5,7 +5,6 @@ from .common.stark_message import (
     StarkProductReviewMessage,
     StarkWebpageMessage,
 )
-from .product.common.favie_product_utils import FavieProductUtils
 from .product.product_detail_adapter.stark_product_detail_adapter import StarkProductDetailAdapter
 from .product.product_list_adapter.stark_product_list_adapter import StarkProductListAdapter
 from .product.product_review_adapter.stark_product_review_adapter import StarkProductReviewAdapter
