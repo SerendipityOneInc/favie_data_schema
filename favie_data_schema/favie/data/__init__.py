1b790e5c28a3bf90897bd7acadff6ada5bd850de
winston-srp <winston@srp.one>
2024-10-11 10:51:06 +0800

    explode enum to interface

--- favie_data_schema/favie/data/__init__.py
+++ favie_data_schema/favie/data/__init__.py
@@ -1,2 +1,4 @@
+from .interface.common.favie_enum import FavieSourceType
 from .interface.product.favie_product import FavieProductDetail, FavieProductReview, FavieProductReviewSummary
+from .interface.product.product_enum import CategoriesMapType, FavieProductDetailStatus
 from .interface.webpage.favie_webpage import FavieWebpage
