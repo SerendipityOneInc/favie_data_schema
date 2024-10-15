52565ff61d4e71000c4299d9976463a227954509
winston-srp <winston@srp.one>
2024-10-15 16:03:53 +0800

    add product data type

--- favie_data_schema/favie/adapter/product/product_detail_adapter/stark_product_detail_convert.py
+++ favie_data_schema/favie/adapter/product/product_detail_adapter/stark_product_detail_convert.py
@@ -6,13 +6,13 @@ from favie_data_common.common.common_utils import CommonUtils
 from lxml import html
 
 from favie_data_schema.favie.adapter.common.html_utils import HtmlUtils
-from favie_data_schema.favie.adapter.common.stark_enum import StarkProductDataType
 from favie_data_schema.favie.adapter.common.stark_message import StarkProductDetailMessage
 from favie_data_schema.favie.adapter.common.stark_message_utils import StarkMessageUtils
 from favie_data_schema.favie.adapter.product.common.currency import CurrencyConverter
 from favie_data_schema.favie.adapter.product.common.favie_product_utils import FavieProductUtils
 from favie_data_schema.favie.data.crawl_data.crawler.common import Source
 from favie_data_schema.favie.data.crawl_data.rainforest.rainforest_product_detail import RainforestProductDetail
+from favie_data_schema.favie.data.interface.common.favie_enum import ProductDataType
 from favie_data_schema.favie.data.interface.product.favie_product import *
 from favie_data_schema.favie.data.interface.product.product_enum import FavieProductDetailStatus
 
@@ -74,7 +74,7 @@ class StarkProductDetailConvert:
         favie_product.f_meta = MetaInfo(
             source_type=str(stark_detail_message.source),
             parser_name=f"{stark_detail_message.parser_name}-adapter",
-            data_type=str(StarkProductDataType.PRODUCT_DETAIL.value),
+            data_type=str(ProductDataType.PRODUCT_DETAIL_CRAWLER.value),
             parses_at=parse_time,
         )
         favie_product.f_status = FavieProductDetailStatus.SKU_NORMAL.name
--- favie_data_schema/favie/data/interface/common/favie_enum.py
+++ favie_data_schema/favie/data/interface/common/favie_enum.py
@@ -7,3 +7,12 @@ class FavieSourceType(IntEnum):
     PRODUCT_REVIEW = 2
     WEB_PAGE = 3
     AD = 4
+
+
+class ProductDataType(IntEnum):
+    UNKNOWN = 0
+    PRODUCT_DETAIL_CRAWLER = 1
+    PRODUCT_LIST_CRAWLER = 2
+    PRODUCT_REVIEW_CRAWLER = 3
+    PRODUCT_CATEGORY_PREDICT = 4
+    PRODUCT_IMAGE_CRAWLER = 5
