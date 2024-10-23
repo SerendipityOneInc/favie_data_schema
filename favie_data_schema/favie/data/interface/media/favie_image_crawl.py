ac69927292745d1e59d7ee966018a47b7b7b9ff2
winston-srp <winston@srp.one>
2024-10-23 19:17:42 +0800

    72 optimize review update (#73)
    * Auto-generate Pydantic models from AVSC
    
    * add meta to image crawl request
    
    * Auto-generate Pydantic models from AVSC
    
    * update version
    
    * Auto-generate Pydantic models from AVSC
    
    * bug fix : none exception
    
    * update version
    
    * update version
    
    * Auto-generate Pydantic models from AVSC
    
    ---------
    
    Co-authored-by: winston-srp <winston-srp@users.noreply.github.com>

--- favie_data_schema/favie/data/interface/media/favie_image_crawl.py
+++ favie_data_schema/favie/data/interface/media/favie_image_crawl.py
@@ -7,6 +7,22 @@ class ImageItem(BaseModel):
     url: Optional[str] = None
     text: Optional[str] = None
 
+class MetaInfo(BaseModel):
+    source_type: Optional[str] = None
+    parser_name: Optional[str] = None
+    parses_at: Optional[str] = None
+    data_type: Optional[str] = None
+    f_categories_update_at: Optional[str] = None
+    f_images_crawl_send_at: Optional[str] = None
+    f_categories_map_type: Optional[int] = None
+    f_categories_map_version: Optional[int] = None
+    f_categories_predict_score: Optional[float] = None
+    source_1_updates_at: Optional[str] = None
+    source_2_updates_at: Optional[str] = None
+    source_3_updates_at: Optional[str] = None
+    source_4_updates_at: Optional[str] = None
+    source_5_updates_at: Optional[str] = None
+
 class FavieImageCrawlResponse(BaseModel):
     source_type: Optional[int] = None
     source_id: Optional[str] = None
@@ -17,4 +33,5 @@ class FavieImageCrawlRequest(BaseModel):
     source_id: Optional[str] = None
     images: Optional[List[ImageItem]] = None
     force: Optional[bool] = None
+    meta_info: Optional[MetaInfo] = None
 
