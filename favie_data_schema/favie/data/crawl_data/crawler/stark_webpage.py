dae6d5fce4235d42c1ec22414be2d68510577db3
ace-srp <ace@srp.one>
2024-12-02 18:45:44 +0800

    添加 subhead

--- favie_data_schema/favie/data/crawl_data/crawler/stark_webpage.py
+++ favie_data_schema/favie/data/crawl_data/crawler/stark_webpage.py
@@ -1,3 +1,11 @@
+'''
+Author: ace-srp ace@srp.one
+Date: 2024-12-02 17:59:25
+LastEditors: ace-srp ace@srp.one
+LastEditTime: 2024-12-02 18:44:33
+FilePath: /favie_data_schema/favie_data_schema/favie/data/crawl_data/crawler/stark_webpage.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 
 from typing import List, Optional
 from pydantic import BaseModel
@@ -15,6 +23,7 @@ class WebpageVideo(BaseModel):
 class WebpageProduct(BaseModel):
     url: Optional[str] = None
     title: Optional[str] = None
+    subhead: Optional[str] = None
     description: Optional[str] = None
     price: Optional[str] = None
     images: Optional[List[WebpageImage]] = None
