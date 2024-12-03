6646c882525904330216d293d79518fd5379336b
winston-srp <winston@srp.one>
2024-12-03 11:47:09 +0800

    add f_image_list,f_system_tags

--- favie_data_schema/favie/data/__init__.py
+++ favie_data_schema/favie/data/__init__.py
@@ -1,3 +1,4 @@
 from .interface.common.favie_enum import CategoriesMapType, FavieDataStatus, FavieSourceType
+from .interface.common.favie_model import FavieTag, ImageItem, MetaInfo
 from .interface.product.favie_product import FavieProductDetail, FavieProductReview, FavieProductReviewSummary
 from .interface.webpage.favie_webpage import FavieWebpage
