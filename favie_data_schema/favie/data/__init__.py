1590f69d79da93fa497015bd7640433f90f5266d
winston-srp <winston@srp.one>
2024-12-03 15:42:38 +0800

    update init

--- favie_data_schema/favie/data/__init__.py
+++ favie_data_schema/favie/data/__init__.py
@@ -1,4 +1,10 @@
 from .interface.common.favie_enum import CategoriesMapType, FavieDataStatus, FavieSourceType
-from .interface.common.favie_model import FavieTag, ImageItem, MetaInfo
+from .interface.common.favie_model import FavieTag, MetaInfo
+from .interface.media.favie_image_crawl import (
+    FavieImageCrawlRequest,
+    FavieImageCrawlResponse,
+    FavieImageItem,
+    RequestImageItem,
+)
 from .interface.product.favie_product import FavieProductDetail, FavieProductReview, FavieProductReviewSummary
 from .interface.webpage.favie_webpage import FavieWebpage
