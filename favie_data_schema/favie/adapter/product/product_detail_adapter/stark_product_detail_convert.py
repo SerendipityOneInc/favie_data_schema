c5d4534742d5eb49eec1dea4c45ef5dda633ae4d
winston-srp <winston@srp.one>
2024-12-30 20:28:16 +0800

    add log

--- favie_data_schema/favie/adapter/product/product_detail_adapter/stark_product_detail_convert.py
+++ favie_data_schema/favie/adapter/product/product_detail_adapter/stark_product_detail_convert.py
@@ -97,8 +97,8 @@ class StarkProductDetailConvert:
         favie_product.f_spu_id = FavieProductUtils.gen_f_spu_id(favie_product)
         favie_product.promotion = StarkProductDetailConvert.get_promotion(crawl_result)
         favie_product.best_seller_rank = StarkProductDetailConvert.get_best_seller_rank(crawl_result)
-        favie_product.variants = None
-        favie_product.variants_str = StarkProductDetailConvert.get_variants_str(stark_detail_message, parse_time)
+        favie_product.variants = StarkProductDetailConvert.get_variants(stark_detail_message, parse_time)
+        # favie_product.variants_str = StarkProductDetailConvert.get_variants_str(stark_detail_message, parse_time)
         favie_product.f_meta = MetaInfo(
             source_type=str(stark_detail_message.source),
             parser_name=f"{stark_detail_message.parser_name}-adapter",
