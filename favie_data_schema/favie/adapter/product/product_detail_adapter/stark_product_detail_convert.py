f688ca04ecad301cd63ae94f1f1217da64276733
winston-srp <winston@srp.one>
2025-02-19 14:38:20 +0800

    bugfix : price is none

--- favie_data_schema/favie/adapter/product/product_detail_adapter/stark_product_detail_convert.py
+++ favie_data_schema/favie/adapter/product/product_detail_adapter/stark_product_detail_convert.py
@@ -73,7 +73,7 @@ class StarkProductDetailConvert:
         price = StarkProductDetailConvert.get_price(stark_detail_message, parse_time)
         favie_product.price = price
         rrp = StarkProductDetailConvert.get_rrp(stark_detail_message, parse_time)
-        favie_product.rrp = rrp if rrp else price if price.value > 0 else None
+        favie_product.rrp = rrp if rrp else price
         favie_product.f_inventory_status = InventoryStatus(in_stock=True if price else False, updates_at=parse_time)
         favie_product.images = StarkProductDetailConvert.get_images(crawl_result)
         favie_product.f_images = None
