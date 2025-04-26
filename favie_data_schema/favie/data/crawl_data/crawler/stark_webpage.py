585cc0bd1ae8f8eb02123c7e04e88e16ae52f0cb
kyle-srp <kyle@srp.one>
2025-04-26 19:19:57 +0800

    feat: optimizeanalytics product data

--- favie_data_schema/favie/data/crawl_data/crawler/stark_webpage.py
+++ favie_data_schema/favie/data/crawl_data/crawler/stark_webpage.py
@@ -66,7 +66,7 @@ class WebpageItem(BaseModel):
     content: Optional[str] = None # text content of the webpage
     content_type: Optional[str] = None
     excerpt: Optional[str] = None
-    comments: Optional[List[str]] = None
+    comments: Optional[List[str]] = None  # [need to be deleted]
     comments_v1: Optional[List[WebpageComment]] = None
     subtitles: Optional[List[str]] = None  # [need to be deleted]
     subtitles_v1: Optional[List[WebpageSubtitleChunk]] = None
