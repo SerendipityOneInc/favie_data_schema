5a646ede9b3fc1f7668cce3840b90be187e0c1ab
kyle-srp <kyle@srp.one>
2025-07-03 09:51:51 +0800

    feat: add spider crawl status (#160)
    * feat: add spider crawl status
    
    * feat: add spider crawl status

--- favie_data_schema/favie/data/crawl_data/crawler/common.py
+++ favie_data_schema/favie/data/crawl_data/crawler/common.py
@@ -1,4 +1,4 @@
-from enum import IntEnum
+from enum import Enum, IntEnum
 class Source(IntEnum):
     # 未知
     UNKNOWN = 0
@@ -12,3 +12,15 @@ class Source(IntEnum):
     EXTERNAL_PURCHASE_DATA = 4
     #内部处理数据，包含离线修复数据
     INTERNAL_PROCESS = 5
+
+
+class SpiderCrawlStatus(Enum):
+    SUCCESS         = (1, "success")            # 网页爬取成功
+    DUPLICATE       = (3, "duplicate")          # 网页重复爬取
+    FAILED          = (-1, "failed")            # 网页爬取失败
+    NOT_FOUND       = (-2, "not found")         # 网页不存在【网页没有出现任何有效信息（商品信息、商品列表等）/第三方API接口应答确认有效信息（商品信息、商品列表等）不存在】
+    PARSE_FAILED    = (-3, "parse failed")      # 解析网页URL失败
+
+    def __init__(self, code, msg):
+        self.code = code
+        self.msg = msg
